c3e109c7109f0c25023c8496cac7aac08fa19781
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-12 11:17:58 +0300

    Task2 sunnipaev 2.1

--- task2.js
+++ task2.js
@@ -1,7 +1,17 @@
-const name = 'Keli'
-const paev = 22 
+const nimi = 'Keli'
+const paev = 22
 const kuu = 12
 const aasta = 1993
-const hobi = 'koerad ja viipekeel'
-const objekt = {name: 'Keli', sunnipaev: paev + '.' + kuu + '.' + aasta,  hobi: hobi}; // object
-console.log(objekt.sunnipaev) 
+const hobi = ('koerad', 'viipekeel')
+const objekt = {nimi: nimi, sunnipaev: paev + '.' + kuu + '.' + aasta ,  hobi: hobi}; // object
+console.log('Sunnipaev 2.0 vastus on: ' + objekt.sunnipaev) 
+
+
+const name = 'Keli'
+const birthday = new Date(1993, 12, 22)
+const day = birthday.getDate()
+const month = birthday.getMonth()+12
+const year = birthday.getFullYear()-1
+const hobby = ('koerad', 'viipekeel')
+const object = {name: 'Keli', birthday: day + '.' + month + '.' + year ,  hobby: hobby}; // objekt
+console.log('Sunnipaev 2.1 vastus on: ' + object.birthday)
