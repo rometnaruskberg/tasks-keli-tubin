0806c96c117b2bf18f102ce9b6b567fdf45ffe54
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-19 16:12:11 +0300

    task4 transport3

--- task4.js
+++ task4.js
@@ -1,4 +1,18 @@
 const color = ['red', 'green', 'white', 'black']
+let comma = '' 
 for (let i = 0; i < color.length; i++) {
-    console.log(color [i])
+    comma += (color[i] + ',')
 }
+console.log(comma) // value with commas
+
+let pluss = '' 
+for (let i = 0; i < color.length; i++) {
+    pluss += (color[i] + '+')
+}
+console.log(pluss) // value with plus
+
+let dot = '' 
+for (let i = 0; i < color.length; i++) {
+    dot += (color[i] + ',')
+}
+console.log(dot + '.') // value end dot
