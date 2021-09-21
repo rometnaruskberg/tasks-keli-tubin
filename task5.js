90c8c97365a2f96425e8dbdc8bea45dcebf277ae
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-21 16:42:14 +0300

    task5 true and false corrections

--- task5.js
+++ task5.js
@@ -1,15 +1,7 @@
-const empty = Boolean('')
-const hi = Boolean('tere')
-const name = Boolean('minu nimi on John')
-console.log(empty)
-console.log(hi)
-console.log(name)
-
-
-const hi3 = 'tere'
-const arr = hi3.split('')
-for (var i = 0; i < arr.length; i++) {
-    arr[i].charAt(0)+ arr[i].toUpperCase() + arr[i].slice(1)
-}
-const hi33 = arr.join('')
-console.log(hi33)
+const empty1 = ''
+const hi2 = 'tere'
+const name3 = 'minu nimi on John'
+const TF1 = Boolean(empty1)
+const TF2 = Boolean(hi2)
+const TF3 = Boolean(name3)
+console.log(!TF1, !TF2, !TF3)
