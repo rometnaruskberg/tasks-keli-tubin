48c0ff480c8595422db838a0c4f78cad2d4736df
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-21 16:52:07 +0300

    task5 capital letter function

--- task5.js
+++ task5.js
@@ -1,7 +1,13 @@
 const empty1 = ''
+const empty = empty1.charAt(0).toUpperCase() + empty1.slice(1)
 const hi2 = 'tere'
+const hi = hi2.charAt(0).toUpperCase() + hi2.slice(1)
 const name3 = 'minu nimi on John'
+const name = name3.charAt(0).toUpperCase() + name3.slice(1)
 const TF1 = Boolean(empty1)
 const TF2 = Boolean(hi2)
 const TF3 = Boolean(name3)
 console.log(!TF1, !TF2, !TF3)
+console.log(empty)
+console.log(hi)
+console.log(name)
