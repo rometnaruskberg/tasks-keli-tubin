d5dbde47b86d5de14cfccd4105935343d6bfd047
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-26 13:41:48 +0300

    tas7 import max and min

--- task7.js
+++ task7.js
@@ -1,11 +1,22 @@
+const fiveRandomArray = []
+
+for (let i = 0; i <= 4; i++){
+    fiveRandomArray.push(randomNumber());
+}
 function randomNumber() {
     n = []
-    return Math.floor(Math.random(n) * 101)
+    return Math.floor(Math.random(n) * 101);
 }
-// console.log(randomNumber())
+function bigNum(fiveRandomArray){
+    return Math.max.apply(null, fiveRandomArray);
+}
+function smallNum(fiveRandomArray){
+    return Math.min.apply(null, fiveRandomArray);
+}
+function even()
 
-let fiveRandom = randomNumber(); 
-    for (let i = 0; i <= 5; randomNumber.length, i++){
-        fiveRandom =+ (randomNumber())
-}
-console.log('Five random numbers are ' + fiveRandom)
+console.log(randomNumber())   // random number
+console.log('Five random numbers are ' + fiveRandomArray) // five random numbers
+console.log(bigNum(fiveRandomArray)) // biggest number
+console.log(smallNum(fiveRandomArray)) // smallest number
+console.log() // even numbers
