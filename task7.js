b92950b78c94b6b2b16f43e43e90137f508ae3bc
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-26 15:06:42 +0300

    tas7 import even 1

--- task7.js
+++ task7.js
@@ -13,9 +13,26 @@ function bigNum(fiveRandomArray){
 function smallNum(fiveRandomArray){
     return Math.min.apply(null, fiveRandomArray);
 }
+function evenNumber(fiveRandomArray){
+    let even = [];
+    for (var i = 0; i < fiveRandomArray; i++){
+        if (fiveRandomArray[i] % 2 === 0){
+            even.push('true');
+        }else (fiveRandomArray[i] % 2 === 1) 
+            even.push('false');
+            var evenNumbers = 0;
+            for (let i = 0; i < even.length; i++){
+                if (even[i] == 'true'){
+                    return evenNumbers++;
+                    }else{}
+                } 
+                return evenNumbers
+    }
+}
 
-console.log(randomNumber())   // random number
-console.log('Five random numbers are ' + fiveRandomArray) // five random numbers
-console.log(bigNum(fiveRandomArray)) // biggest number
-console.log(smallNum(fiveRandomArray)) // smallest number
-//console.log() // even numbers
+
+console.log(randomNumber())
+console.log('Five random numbers are ' + fiveRandomArray)
+console.log(bigNum(fiveRandomArray))
+console.log(smallNum(fiveRandomArray))
+console.log('Even numbers are ' + evenNumber(fiveRandomArray))
