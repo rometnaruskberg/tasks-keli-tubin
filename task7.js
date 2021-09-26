6dce7bec8ba867558bd83dc2e578d87badc335fe
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-26 12:54:26 +0300

    tas7 import 2

--- task7.js
+++ task7.js
@@ -2,4 +2,10 @@ function randomNumber() {
     n = []
     return Math.floor(Math.random(n) * 101)
 }
-console.log(randomNumber())
+// console.log(randomNumber())
+
+let fiveRandom = randomNumber(); 
+    for (let i = 0; i <= 5; randomNumber.length, i++){
+        fiveRandom =+ (randomNumber())
+}
+console.log('Five random numbers are ' + fiveRandom)
