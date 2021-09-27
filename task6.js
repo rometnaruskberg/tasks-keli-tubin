9ff7ca7e69cb90b3b96c8123286e275ec16387a4
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-27 20:55:42 +0300

    task6 Hi! if n is more than 10 final

--- task6.js
+++ task6.js
@@ -1,12 +1,17 @@
 function greet(n) {
     if (!n) {
         console.log('Hi!')
-    } else if (n < 10) {
-        for (let i = 0; i < n; i++)
-           console.log('Hello!')
+    } else if (n > 10) {
+        for (let i = 0; i < n; i++) {
+            console.log('Hi!')
+        }
+    } else {
+        for (let i = 0; i < n; i++) {
+            console.log('Hello!')
         }
     }
-}
+}   
+
 greet()
 greet(2)
 greet(3)
