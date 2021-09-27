ccba14b532469122463355cd4b9396d26f0b91bb
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-27 20:34:13 +0300

    task6 Hi! if n is empty

--- task6.js
+++ task6.js
@@ -1,20 +1,13 @@
 function greet(n) {
     if (!n) {
-        console.log(undefined)
+        console.log('Hi!')
     } else {
         for (let i = 0; i < n; i++) {
-            console.log('Hello!')
+           console.log('Hello!')
         }
     }
 }
 greet()
 greet(2)
 greet(3)
-greet(12)    
-    
-//const hello1 = 'Hello! '
-//let n1 = [ , 2, 3, 12]
-//console.log(hello1.repeat(n1[0]))
-//console.log(hello1.repeat(n1[1]))
-//console.log(hello1.repeat(n1[2]))
-//console.log(hello1.repeat(n1[3]))
+greet(12)
