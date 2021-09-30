b6ddb02a3e02a51255955f35a9461af1991f0517
KeliTubin <Keli.Tubin@vikk.ee>
2021-09-30 20:20:21 +0300

    task4 final upload

--- task4.js
+++ task4.js
@@ -13,10 +13,16 @@ console.log(pluss) // value with plus
 
 let dot = '' 
 for (let i = 0; i < color.length; i++) {
-    dot += (color[i] + ',')
+    const ending = i + 1 === color.length
+    if (ending) {
+        dot += color[i] + '.'
+    }else{
+        dot += (color[i] + ',')
+    }
 }
-console.log(dot + '.') // value end dot
+console.log(dot) // value end dot
 console.log('* 4.0 end *')
+
 
 const comspace = color.join(', ')
 const com = color.join(',')
