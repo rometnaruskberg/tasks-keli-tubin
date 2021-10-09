c6c0d05f2465e6728bd4bd06721793e3bc94933e
KeliTubin <Keli.Tubin@vikk.ee>
2021-10-09 10:53:52 +0300

    task1 final upload

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 const a = 5
 let b = 10
 let c = a + b
-console.log(c) // vastus on 5 + 10 = 15
-b = 5
+console.log(c) // answer is 5 + 10 = 15 (math calculation)
+b = '5'
 let d = a + b
-console.log(d) // vastus on 5 + 5 = 10
+console.log(d) // answer is '5' + 5 = 55 (two variables added next to each other)
